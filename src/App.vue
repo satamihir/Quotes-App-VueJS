a5cbdd0b3c9f70091d9138b07984de717818f5fe
Mihir Sata <smihir@narola.local>
2020-07-02 17:56:31 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="container">
+    <appHeader :quoteCount= "quotes.length" :maxQuotes= "maxQuotes"></appHeader>
+    <appNewQuote @addNewQuote="newQuote"></appNewQuote>
+    <appQuoteGrid :quotes="quotes" @quoteWasDeleted="deleteQuote"></appQuoteGrid>
+
+    <div class="row">
+        <div class= "col-sm-12 text-center">
+            <div class="alert alert-info">Info: Click on a quote to delete it!</div>
+        </div>
+    </div>
+    </div>
+</template>
+
+<script>
+
+import QuoteGrid from './components/QuoteGrid';
+import NewQuote from './components/NewQuote';
+import Header from './components/Header';
+    export default {
+        data(){
+            return{
+                quotes: [
+                    'Just a Quote'
+                ],
+                maxQuotes: 10
+            }
+           
+        },
+        methods:{
+            newQuote(quote){
+                if(this.quotes.length >= this.maxQuotes){
+                    return alert('Please delete some of the quotes');
+                }
+                this.quotes.push(quote);
+            },
+            deleteQuote(index){
+                this.quotes.splice(index,1);
+            }
+        },
+         
+        
+        components: {
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote,
+            appHeader: Header   
+        }
+    }
+    
+    
+</script>
+
+<style>
+</style>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="row">
+        <appQuote v-for="(quote,index) in quotes" @click.native="deleteQuote(index)">{{quote}}</appQuote>
+    </div>
+</template>
+
+<script>
+import Quote from './Quote';
+export default {
+    props: ['quotes'],
+    methods:{
+        deleteQuote(index) {
+            this.$emit('quoteWasDeleted',index);
+        }
+    },
+    components: {
+    appQuote: Quote
+    }
+
+}
+
+
+
+</script>
+
+<style scoped>
+
+</style>
